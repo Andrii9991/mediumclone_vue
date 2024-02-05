b5d322b11d7094d91d4a8d7c7c6aaba27f25cc68
Andrii9991 <117731577+Andrii9991@users.noreply.github.com>
2024-02-05 23:43:07 +0200

    fix (#1)

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,11 +1,92 @@
 <template>
-  <div class="register">
-    <h1>Register page</h1>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign up</h1>
+          <p class="text-xs-center">
+            <router-link :to="{ name: 'login' }">
+              Need an account?
+            </router-link>
+          </p>
+          <ValidationErrors
+            v-if="validationErrors"
+            :validationErrors="validationErrors"
+          />
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Username"
+                v-model="username"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Email"
+                v-model="email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+                v-model="password"
+              />
+            </fieldset>
+            <button
+              class="btn btn-lg btn-primary pull-xs-right"
+              :disabled="isSubmitting"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ValidationErrors from "@/components/ValidationErrors.vue";
 export default {
+  components: {
+    ValidationErrors,
+  },
   name: "RegisterPage",
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+    };
+  },
+  computed: {
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting;
+    },
+    validationErrors() {
+      return this.$store.state.auth.validationErrors;
+    },
+  },
+
+  methods: {
+    onSubmit() {
+      this.$store
+        .dispatch("auth/register", {
+          email: this.email,
+          username: this.email,
+          password: this.password,
+        })
+        .then((result) => {
+          console.log("result from register action", result);
+          this.$router.push({ name: "home" });
+        });
+    },
+  },
 };
 </script>
