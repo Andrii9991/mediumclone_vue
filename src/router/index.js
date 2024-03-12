e5d30c736d8c3ca6cfd0d20f15025d05ef34db1f
Andrii9991 <andriymyshchyshyn6@gmail.com>
2024-03-12 12:25:52 +0200

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import GlobalFeed from "../views/GlobalFeed.vue";
+import YourFeed from "../views/YourFeed.vue";
+import TagFeed from "../views/TagFeed.vue";
 import RegisterPage from "../views/RegisterPage.vue";
 import LoginPage from "../views/LoginPage.vue";
 import TheArticle from "@/views/TheArticle.vue";
@@ -17,6 +19,51 @@ const routes = [
     path: "/login",
     name: "login",
     component: LoginPage,
+  },
+  {
+    path: "/feed",
+    name: "yourFeed",
+    component: YourFeed,
+  },
+  {
+    path: "/tags/:slug",
+    name: "tag",
+    component: TagFeed,
+  },
+  {
+    path: "/article/new",
+    name: "createArticle",
+    component: GlobalFeed,
+  },
+  {
+    path: "/article/:slug",
+    name: "article",
+    component: TheArticle,
+  },
+  {
+    path: "/article/:slug/edit",
+    name: "editArticle",
+    component: GlobalFeed,
+  },
+  {
+    path: "/settings",
+    name: "settings",
+    component: GlobalFeed,
+  },
+  {
+    path: "/profiles/:slug",
+    name: "userProfile",
+    component: GlobalFeed,
+  },
+  {
+    path: "/profiles/:slug/favorites",
+    name: "userProfileFavorites",
+    component: GlobalFeed,
+  },
+  {
+    path: "/",
+    name: "globalFeed",
+    component: GlobalFeed,
   },
 ];
 
